0d4d0d9e5e63faa39f1c64ddc1c1b1cb89441e3f
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-14 20:49:47 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,19 @@
 class Triangle
   attr_accessor :side_a, :side_b, :side_c, :sides
   
-  def initialize(sideA, sideB, sideC)
+  def initialize(side_a, side_b, side_c)
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
-    @sides = [side_a, side_b, side_c].sort
+    @sides = [@side_a, @side_b, @side_c].sort
   end
   
-   def illegal_triangle
-    @sides.any? {|side| side <= 0} || @sides[0] + @sides[1] <= @sides[2]
+  def illegal_triangle?
+    @sides.any? {|side| side <= 0} || sides[0] + sides[1] <= sides[2]
   end 
   
   def kind
-    if illegal_triangle
+    if illegal_triangle?
       raise TriangleError
     elsif @sides.uniq.length == 1
       :equilateral
@@ -26,9 +26,6 @@ class Triangle
   
 
   class TriangleError < StandardError
-    def message
-      "That is not a triangle."
-    end
   end
   
 end

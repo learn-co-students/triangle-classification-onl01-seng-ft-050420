77b446d6abb490fd6b14d4f924131d6c4f6b836c
mkopsho <michael.kopsho@gmail.com>
2020-05-20 13:37:33 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind # Too many comparators here for my taste. How to refactor?
+    if side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b
+      raise TriangleError
+    elsif side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    elsif side_a != side_b && side_b != side_c
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end

b1f3b501d98561d71740d2a66b3af4115fe0bc71
Aaron Schuyler <asky28@gmail.com>
2020-05-20 20:35:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end 
+  def kind 
+    if @one >= (@two + @three) || @two >= (@one + @three) || @three >= (@one + @two) || @one <= 0 || @two <= 0 || @three <= 0
+      raise Triangle::TriangleError
+    elsif @one != @three && @three != @two && @one != @two
+      :scalene
+    elsif (@one == @two && @three != @one) || (@one == @three && @two != @one) || (@two == @three && @one != @three)
+      :isosceles
+    elsif @one == @two && @two == @three && @one != 0
+      :equilateral
+    end
+  end
+  class TriangleError < StandardError
+  def message
+    "There was an error with your triangle."
+  end
 end
+end
+
